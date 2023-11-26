311ced957ec49617476a7f39fb5bfae3a85ce83d
efmct30 <enzofranchesco.ec@gmail.com>
2023-11-25 19:12:15 -0500

    PREGUNTA 3

--- src/app/menu-principal/submenu2/submenu2.component.ts
+++ src/app/menu-principal/submenu2/submenu2.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./submenu2.component.css']
 })
 export class Submenu2Component {
+  montoPrestamo: number = 0;
+  cuotas: number = 0;
+  montoPorCuota: number = 0;
 
+  calcularPrestamo() {
+    if (this.montoPrestamo > 5000) {
+      this.cuotas = 3;
+    } else if (this.montoPrestamo < 1000) {
+      this.cuotas = 1;
+    } else if (this.montoPrestamo >= 2000 && this.montoPrestamo <= 3000) {
+      this.cuotas = 2;
+    } else {
+      this.cuotas = 5;
+    }
+
+    const interes = this.montoPrestamo < 4000 ? 0.12 : 0.10;
+    const montoTotal = this.montoPrestamo * (1 + interes);
+
+    this.montoPorCuota = montoTotal / this.cuotas;
+  }
 }
